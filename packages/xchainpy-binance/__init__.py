81fea3cfdbf2c9555a6d244676643a80ee2545a8
Adel <adel.aboutalebi@hotmail.com>
2021-01-24 13:00:13 +0330

    add three foldres (xchainpy-binance, xchainpy-client, xchainpy-crypto)

---
+++ packages/xchainpy-binance/__init__.py
@@ -0,0 +1,5 @@
+"""Binance Module for XChainPY Clients
+.. moduleauthor:: Thorchain
+"""
+
+__version__ = '0.1'
---
+++ packages/xchainpy-crypto/__init__.py
@@ -0,0 +1,5 @@
+"""The XCHAIN CRYPTO package is a crypto package used by all XCHAIN clients
+.. moduleauthor:: Thorchain
+"""
+
+__version__ = '0.1'
