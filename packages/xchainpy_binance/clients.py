ee43d67281d5e1d548241f54ec0d89e5a7dcd7ba
Adel <adel.aboutalebi@hotmail.com>
2021-01-26 21:01:16 +0330

    add setnetwork setphrase getAddress getPrivatekey methods to binance client

--- packages/xchainpy_binance/clients.py
+++ packages/xchainpy_binance/clients.py
@@ -3,46 +3,60 @@ from binance_chain.constants import KlineInterval
 from binance_chain.environment import BinanceEnvironment
 
 from xchainpy_client import interface
-from xchainpy_crypto import crypto
+from xchainpy_crypto import crypto as xchainpy_crypto
+from xchainpy_binance import crypto
+from xchainpy_binance import utils
 
-class Client(interface.IXChainClient):
+class Client(interface.IXChainClient): # create an interface for binance methods (getprivatekey, getClientUrl and ...)
+
+    #add these params:
+        # self.privateKey, self.privateKey, self.address, self.client
+    phrase = address = network = ''
+    privateKey = client = None
+
     def init(self, phrase, network = 'testnet'):
-        self.phrase = phrase
-        self.network = network
-        
-        # choose network (testnet or mainnet)
-        if self.network == 'testnet':
-            # initialise with Testnet environment
-            testnet_env = BinanceEnvironment.get_testnet_env()
-            self.client = AsyncHttpApiClient(env=testnet_env)
-        elif self.network == 'mainnet':
-            # Alternatively pass no env to get production
-            self.client = AsyncHttpApiClient()
-        else: 
-            #thorw err
-            pass
+        self.setPhrase(phrase)
+        self.setNetwork(phrase)
 
-        #set phrase ():
-            # check phrase validity (xchainpy_crypto)
+    def getPrivateKey(self):
+        if not self.privateKey:
+            if not self.phrase:
+                # thorw an err
+                pass
 
-    def getAddress(self): 
+            self.privateKey = crypto.mnemonicToPrivateKey(self.phrase) # passPhrase ?
+        return self.privateKey
+
+    def getAddress(self):
         if not self.address:
-            address = None
-        return 
+            self.address = crypto.privateKeyToAddress(self.getPrivateKey(), utils.getPrefix(self.network))
+        return self.address
 
-        
+
     def setPhrase(self, phrase: str):
         if not self.phrase or self.phrase != phrase:
-            if not crypto.validatePhrase(phrase):
+            if not xchainpy_crypto.validatePhrase(phrase):
                 pass # throw an err
             self.phrase = phrase
             self.privateKey = None
             self.address = ''
 
-        return getAddress()
+        return self.getAddress()
 
     def setNetwork(self, network: str):
-        pass
+        if not self.network and not self.client:
+            # choose network (testnet or mainnet)
+            if self.network == 'testnet':
+                # initialise with Testnet environment
+                testnet_env = BinanceEnvironment.get_testnet_env()
+                self.client = AsyncHttpApiClient(env=testnet_env)
+            elif self.network == 'mainnet':
+                # Alternatively pass no env to get production
+                self.client = AsyncHttpApiClient()
+            else: 
+                #thorw err
+                pass
+        return self.client
 
     def getBalance(self, address: str, asset):
         pass
