3dd5e0a117cd6e3e58448bf58ab5a975b9dab765
Adel <adel.aboutalebi@hotmail.com>
2021-01-29 01:15:04 +0330

    add docstring

--- packages/xchainpy_crypto/crypto.py
+++ packages/xchainpy_crypto/crypto.py
@@ -3,5 +3,11 @@ from bip_utils import Bip39MnemonicValidator
 
 # Validate a mnemonic string by verifying its checksum
 def validate_phrase(phrase: str):
+    """Check validity of mnemonic (phrase)
+
+    :param phrase: a phrase
+    :type phrase: str
+    :returns: is the phrase valid or not (true or false)
+    """
     is_valid = Bip39MnemonicValidator(phrase).Validate()
     return is_valid
