4b24046457501ffc99fee684e7068654eb8a2384
Adelapt98 <adel.aboutalebi@hotmail.com>
2021-02-11 00:49:51 +0330

    added some btc client functions

--- xchainpy/xchainpy_bitcoin/client.py
+++ xchainpy/xchainpy_bitcoin/client.py
@@ -1,4 +1,5 @@
 
+from xchainpy.xchainpy_crypto.crypto import validate_phrase
 from xchainpy.xchainpy_client.interface import IXChainClient
 
 
@@ -17,12 +18,36 @@ class Client(IBitcoinClient,IXChainClient):
     node_url = node_api_key = phrase = net = ''
 
     def __init__(self, phrase , network='testnet' , node_url = '' , node_api_key = ''):
-        
         self.set_network(network)
+        self.set_node_url(node_url)
+        self.set_node_api_key(node_api_key)
         self.set_phrase(phrase)
 
     def set_network(self, network: str):
         if not network :
             raise Exception("Network must be provided")
         else:
-            self.net = network
+            if not network in ['testnet' , 'mainnet']:
+                raise Exception('Invalid network')
+            else:
+                self.net = network
+    
+    def set_node_url(self , url : str):
+        if not url:
+            raise Exception("Node url must be provided")
+        else:
+            self.node_url = url
+
+    def set_node_api_key(self , node_api_key):
+        if not node_api_key:
+            raise Exception("Node API key must be provided")
+        else:
+            self.set_node_api_key = node_api_key
+    
+    def set_phrase(self , phrase : str):
+        if validate_phrase(phrase):
+            self.phrase = phrase
+            address = self.get_address()
+            return address
+        else:
+            raise Exception("Invalid Phrase")
