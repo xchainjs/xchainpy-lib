3f877409411e34fd41e75dc40ebafe3cb17853cf
Adel <adel.aboutalebi@hotmail.com>
2021-02-13 00:06:45 +0330

    get_transaction, get_transaction_data implemented

--- xchainpy/xchainpy_bitcoin/client.py
+++ xchainpy/xchainpy_bitcoin/client.py
@@ -5,7 +5,11 @@ from xchainpy.xchainpy_bitcoin import utils
 
 from bitcoinlib.wallets import Wallet
 from bitcoinlib.wallets import *
-from bitcoinlib.services.services import *
+from xchainpy.xchainpy_client.models import tx_types
+from xchainpy.xchainpy_bitcoin import sochain_api
+from xchainpy.xchainpy_client.models.balance import Balance
+from xchainpy.xchainpy_util.asset import Asset
+
 
 class IBitcoinClient():
     def derive_path(self):
@@ -83,3 +87,53 @@ class Client(IBitcoinClient,IXChainClient):
             return self.address
         raise Exception('Phrase must be provided')
 
+    async def get_balance(self, address:str=None):
+        try:
+            amount = await utils.get_balance(self.service, address or self.address)
+            amount = round(amount * 10 ** -8, 8) 
+            balance = Balance(Asset.from_str('BTC.BTC'), amount)
+            return balance
+        except Exception as err:
+            raise Exception(str(err))
+
+    async def get_transactions(self, params: tx_types.TxHistoryParams):
+        try:
+            transactions = await sochain_api.get_transactions(self.net, self.address)
+            total = transactions['total_txs']
+            txs = []
+            for tx in transactions['txs']:
+                tx = await sochain_api.get_tx(self.net, tx['txid'])
+                tx = utils.parse_tx(tx)
+                txs.append(tx)
+
+            return {
+                'total': total,
+                'tx': txs
+            }
+        except Exception as err:
+            raise Exception(str(err))
+
+
+    async def get_transaction_data(self, tx_id:str):
+        try:
+            tx = await sochain_api.get_tx(self.net, tx_id)
+            tx = utils.parse_tx(tx)
+            return tx
+        except Exception as err:
+            raise Exception(str(err))
+
+
+    async def get_fees_with_rates(self):
+        tx_fee = await sochain_api.get_suggested_tx_fee()
+        return {
+                'rates': {
+                    'fast': tx_fee * 5,
+                    'fastest': tx_fee * 1,
+                    'average': tx_fee * 0.5
+                },
+                'multi': {
+                    'fast': tx_fee,
+                    'fastest': tx_fee,
+                    'average': tx_fee
+                }
+            }
--- xchainpy/xchainpy_bitcoin/utils.py
+++ xchainpy/xchainpy_bitcoin/utils.py
@@ -1,5 +1,13 @@
 from typing import Union
+import asyncio
 from xchainpy.xchainpy_bitcoin.models.common import DerivePath
+from bitcoinlib.services.services import *
+from xchainpy.xchainpy_util.asset import Asset
+from xchainpy.xchainpy_client.models.balance import Balance
+from xchainpy.xchainpy_util.chain import BTCCHAIN
+from xchainpy.xchainpy_client.models import tx_types
+import datetime
+
 
 TX_EMPTY_SIZE = 4 + 1 + 1 + 4 #10
 TX_INPUT_BASE = 32 + 4 + 1 + 4 #41
@@ -11,3 +19,28 @@ DUST_THRESHOLD = 1000
 
 def get_derive_path(index:int=0):
     return DerivePath(index=index)
+
+async def get_balance(service:Service, address:str):
+    get_balance_async = asyncio.coroutine(service.getbalance)
+    return await get_balance_async(address)
+
+# async def get_transactions(service:Service, address:str, limit=10000):
+#     get_transactions_async = asyncio.coroutine(service.gettransactions)
+#     return await get_transactions_async(address, limit)
+
+def parse_tx(tx):
+    """Parse tx
+
+    :param tx: The transaction to be parsed
+    :type tx: str
+    :returns: The transaction parsed from the binance tx
+    """
+    asset = Asset.from_str(f'{BTCCHAIN}.BTC')
+    tx_from = [tx_types.TxFrom(i['address'], i['value']) for i in tx['inputs']]
+    tx_to = [tx_types.TxTo(i['address'], i['value']) for i in tx['outputs']]
+    tx_date = datetime.datetime.fromtimestamp(tx['time'])
+    tx_type = 'transfer'
+    tx_hash = tx['txid']
+
+    tx = tx_types.TX(asset, tx_from, tx_to, tx_date, tx_type, tx_hash)
+    return tx
