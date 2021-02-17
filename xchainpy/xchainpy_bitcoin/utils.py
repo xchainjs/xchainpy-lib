3256ba1d206ab984a0ac690f00ea8d2c478f0314
Adel <adel.aboutalebi@hotmail.com>
2021-02-17 10:18:01 +0330

    fees calculation implemented

--- xchainpy/xchainpy_bitcoin/utils.py
+++ xchainpy/xchainpy_bitcoin/utils.py
@@ -1,12 +1,14 @@
-from typing import Union
+from typing import List, Optional, Union
 import asyncio
-from xchainpy.xchainpy_bitcoin.models.common import DerivePath
+from xchainpy.xchainpy_bitcoin.const import MIN_TX_FEE
+from xchainpy.xchainpy_bitcoin.models.common import DerivePath, UTXO
 from bitcoinlib.services.services import *
 from xchainpy.xchainpy_util.asset import Asset
 from xchainpy.xchainpy_client.models.balance import Balance
 from xchainpy.xchainpy_util.chain import BTCCHAIN
 from xchainpy.xchainpy_client.models import tx_types
 import datetime
+import binascii
 
 
 TX_EMPTY_SIZE = 4 + 1 + 1 + 4 #10
@@ -36,3 +38,28 @@ def parse_tx(tx):
 
     tx = tx_types.TX(asset, tx_from, tx_to, tx_date, tx_type, tx_hash)
     return tx
+
+
+def calc_fee(fee_rate , memo=''):
+    compiled_memo = compile_memo(memo) if memo else None
+    fee = get_fee([] , fee_rate , compiled_memo)
+    return fee
+
+def compile_memo(memo : str):
+    # data = memo.encode('utf-8').hex()
+    data = bytes(memo , 'utf-8')
+    # raw = binascii.a2b_hex(data)
+    return data
+
+def get_fee(inputs : List[UTXO] , fee_rate : float , data : Optional[bytes] = None):
+    lst_reduce = functools.reduce(lambda a , x : a + input_bytes(x),inputs) if len(inputs) > 0 else 0 
+    sum = TX_EMPTY_SIZE + lst_reduce + len(inputs) + TX_OUTPUT_BASE + TX_OUTPUT_PUBKEYHASH + TX_OUTPUT_BASE + TX_OUTPUT_PUBKEYHASH
+    if data:
+        sum = sum + TX_OUTPUT_BASE + len(data)
+    fee = sum * fee_rate
+    result = fee if fee > MIN_TX_FEE else MIN_TX_FEE
+    return result
+
+def input_bytes(input : UTXO):
+    result = TX_INPUT_BASE + (len(input.witness_utxo['script']) if input.witness_utxo['script'] else TX_INPUT_PUBKEYHASH)
+    return result
