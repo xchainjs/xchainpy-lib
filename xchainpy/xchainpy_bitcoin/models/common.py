7d27aef5111d0c0ed9054acaad9876da66883648
Adel <adel.aboutalebi@hotmail.com>
2021-02-17 10:14:14 +0330

    add utxo model to bitcoin module

--- xchainpy/xchainpy_bitcoin/models/common.py
+++ xchainpy/xchainpy_bitcoin/models/common.py
@@ -19,3 +19,43 @@ class DerivePath:
     @testnet.setter
     def testnet(self, testnet):
         self._testnet = testnet
+
+
+class UTXO:
+    def __init__(self, hash : str , index : int , witness_utxo , tx_hex : str):
+      self._hash = hash
+      self._index = index
+      self._witness_utxo = witness_utxo
+      self._tx_hex = tx_hex
+    
+    @property
+    def hash(self):
+        return self._hash
+
+    @hash.setter
+    def hash(self , hash):
+        self._hash = hash
+    
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self , index):
+        self._index = index
+
+    @property
+    def witness_utxo(self):
+        return self._witness_utxo
+
+    @witness_utxo.setter
+    def witness_utxo(self , witness_utxo):
+        self._witness_utxo = witness_utxo
+
+    @property
+    def tx_hex(self):
+        return self._tx_hex
+
+    @tx_hex.setter
+    def tx_hex(self , tx_hex):
+        self._tx_hex = tx_hex
