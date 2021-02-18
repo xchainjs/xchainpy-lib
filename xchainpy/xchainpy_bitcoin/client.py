3375180810b077a4ee2b3c94e41e01d4feb30901
Adel <adel.aboutalebi@hotmail.com>
2021-02-18 16:12:53 +0330

    implement transfer using sochain api

--- xchainpy/xchainpy_bitcoin/client.py
+++ xchainpy/xchainpy_bitcoin/client.py
@@ -1,4 +1,5 @@
 
+import asyncio
 from xchainpy.xchainpy_crypto.crypto import validate_phrase
 from xchainpy.xchainpy_client.interface import IXChainClient
 from xchainpy.xchainpy_bitcoin import utils
@@ -14,19 +15,23 @@ from xchainpy.xchainpy_util.asset import Asset
 class IBitcoinClient():
     def derive_path(self):
         pass
+
     def get_fees_with_rates(self):
         pass
-    def get_fees_with_memo(self): 
-        pass
-    def get_fee_rates(self): 
+
+    def get_fees_with_memo(self):
         pass
 
-class Client(IBitcoinClient,IXChainClient):
+    def get_fee_rates(self):
+        pass
+
+
+class Client(IBitcoinClient, IXChainClient):
 
     node_url = node_api_key = phrase = net = address = ''
     wallet = None
 
-    def __init__(self, phrase , network='testnet'):
+    def __init__(self, phrase, network='testnet'):
         """
         :param phrase: a phrase (mnemonic)
         :type phrase: str
@@ -37,7 +42,6 @@ class Client(IBitcoinClient,IXChainClient):
         self.set_phrase(phrase)
         self.service = Service(network=self.get_network())
 
-
     def set_network(self, network: str):
         """Set/update the current network
 
@@ -47,14 +51,14 @@ class Client(IBitcoinClient,IXChainClient):
         :raises: raises if network not provided
         :raises: `Invalid network' if the given network is invalid
         """
-        if not network :
+        if not network:
             raise Exception("Network must be provided")
         else:
-            if not network in ['testnet' , 'mainnet']:
+            if not network in ['testnet', 'mainnet']:
                 raise Exception('Invalid network')
             else:
                 self.net = network
-    
+
     def set_wallet(self, phrase):
         """Set/update the current wallet
 
@@ -64,10 +68,11 @@ class Client(IBitcoinClient,IXChainClient):
         """
         # self.wallet = Wallet("Wallet")
         wallet_delete_if_exists('Wallet', force=True)
-        self.wallet = Wallet.create("Wallet", keys=self.phrase , witness_type='segwit', network=self.get_network())
+        self.wallet = Wallet.create(
+            "Wallet", keys=self.phrase, witness_type='segwit', network=self.get_network())
         return self.wallet
 
-    def set_phrase(self , phrase : str):
+    def set_phrase(self, phrase: str):
         """Set/Update a new phrase
 
         :param phrase: A new phrase
@@ -83,7 +88,6 @@ class Client(IBitcoinClient,IXChainClient):
         else:
             raise Exception("Invalid Phrase")
 
-
     def purge_client(self):
         """Purge client
         """
@@ -98,7 +102,6 @@ class Client(IBitcoinClient,IXChainClient):
     def derive_path(self):
         return utils.get_derive_path().testnet if self.net == 'testnet' else utils.get_derive_path().mainnet
 
-
     def get_address(self):
         """Get the current address
 
@@ -107,14 +110,14 @@ class Client(IBitcoinClient,IXChainClient):
         """
         if self.phrase:
             self.address = self.wallet.get_key().address
-        
+
             if not self.address:
                 raise Exception('Address not defined')
 
             return self.address
         raise Exception('Phrase must be provided')
 
-    async def get_balance(self, address:str=None):
+    async def get_balance(self, address: str = None):
         """Get the BTC balance of a given address
 
         :param address: By default, it will return the balance of the current wallet. (optional)
@@ -152,8 +155,7 @@ class Client(IBitcoinClient,IXChainClient):
         except Exception as err:
             raise Exception(str(err))
 
-
-    async def get_transaction_data(self, tx_id:str):
+    async def get_transaction_data(self, tx_id: str):
         """Get the transaction details of a given transaction id
 
         if you want to give a hash that is for mainnet and the current self.net is 'testnet',
@@ -170,9 +172,8 @@ class Client(IBitcoinClient,IXChainClient):
         except Exception as err:
             raise Exception(str(err))
 
-    
-    #todo: complete this method by adding calc_fee(memo) method
-    async def get_fees_with_rates(self, memo:str=''):
+    # todo: complete this method by adding calc_fee(memo) method
+    async def get_fees_with_rates(self, memo: str = ''):
         """Get the rates and fees
 
         :param memo: The memo to be used for fee calculation (optional)
@@ -181,17 +182,17 @@ class Client(IBitcoinClient,IXChainClient):
         """
         tx_fee = await sochain_api.get_suggested_tx_fee()
         return {
-                'rates': {
-                    'fast': tx_fee * 5,
-                    'fastest': tx_fee * 1,
-                    'average': tx_fee * 0.5
-                },
-                'fees': {
-                    'fast': tx_fee,
-                    'fastest': tx_fee,
-                    'average': tx_fee
-                }
+            'rates': {
+                'fast': tx_fee * 5,
+                'fastest': tx_fee * 1,
+                'average': tx_fee * 0.5
+            },
+            'fees': {
+                'fast': tx_fee,
+                'fastest': tx_fee,
+                'average': tx_fee
             }
+        }
 
     async def get_fees(self):
         """Get the current fees
@@ -204,8 +205,7 @@ class Client(IBitcoinClient,IXChainClient):
         except Exception as err:
             raise Exception(str(err))
 
-
-    async def get_fees_with_memo(self, memo:str):
+    async def get_fees_with_memo(self, memo: str):
         """Get the fees for transactions with memo
         If you want to get `fees` and `fee_rates` at once, use `get_fees_with_rates` method
 
@@ -231,9 +231,39 @@ class Client(IBitcoinClient,IXChainClient):
         except Exception as err:
             raise Exception(str(err))
 
-    def transfer(self, amount, recipient, memo:str=None, fee=None):
-        self.wallet.utxos_update()
-        tx = self.wallet.send_to(to_address=recipient, amount=amount)
-        self.wallet.info()
-        self.wallet.scan()
-        return tx
+
+    def validate_address(self, network, address):
+        """Validate the given address
+
+        :param network: testnet or mainnet
+        :type network: str
+        :param address: address
+        :type address: str
+        :returns: True or False
+        """
+        return utils.validate_address(network, address)
+
+
+    async def transfer(self, amount, recipient, memo:str=None, fee_rate=None):
+        """Transfer BTC
+
+        :param network: The transfer options
+
+        :param amount: amount of BTC to transfer (don't multiply by 10**8)
+        :type amount: int, float, decimal
+        :param recipient: destination address
+        :type recipient: str
+        :param memo: optional memo for transaction
+        :type memo: str
+        :param fee_rate: fee rates for transaction
+        :type fee_rate: int
+        :returns: the transaction hash
+        """
+        
+        t, utxos = await utils.build_tx(amount=int(amount*10**8), recipient=recipient, memo=memo, fee_rate=fee_rate,
+                                        sender=self.get_address(), network=self.net)
+
+        hdkey = self.wallet.get_key().key()
+        t.sign(hdkey.private_byte)
+
+        return await utils.broadcast_tx(self.net, t.raw_hex())
