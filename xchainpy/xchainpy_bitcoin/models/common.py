a572c9b2ffc03be8fc939b815f9101ca47ea6249
Adel <adel.aboutalebi@hotmail.com>
2021-02-18 10:20:10 +0330

    Witness_UTXO model implemented

--- xchainpy/xchainpy_bitcoin/models/common.py
+++ xchainpy/xchainpy_bitcoin/models/common.py
@@ -22,11 +22,20 @@ class DerivePath:
 
 
 class UTXO:
-    def __init__(self, hash : str , index : int , witness_utxo , tx_hex : str):
+    def __init__(self, hash : str , index : int , witness_utxo , tx_hex:str=None):
       self._hash = hash
       self._index = index
       self._witness_utxo = witness_utxo
       self._tx_hex = tx_hex
+
+    @classmethod
+    def from_sochain_utxo(cls, utxo):
+        hash = utxo['txid']
+        index = utxo['output_no']
+        value = int(float(utxo['value']) * 10 ** 8)
+        script =  bytearray.fromhex(utxo['script_hex']) #utxo['script_hex']
+        witness_utxo = Witness_UTXO(value, script)
+        return UTXO(hash, index, witness_utxo)
     
     @property
     def hash(self):
@@ -59,3 +68,25 @@ class UTXO:
     @tx_hex.setter
     def tx_hex(self , tx_hex):
         self._tx_hex = tx_hex
+
+
+class Witness_UTXO:
+    def __init__(self, value, script):
+      self._value = value
+      self._script = script
+    
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self , value):
+        self._value = value
+    
+    @property
+    def script(self):
+        return self._script
+
+    @script.setter
+    def script(self , script):
+        self._script = script
