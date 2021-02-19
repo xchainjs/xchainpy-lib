e150e1ba921846fe4f9ba775d7d3f58d85553219
Adelapt98 <adel.aboutalebi@hotmail.com>
2021-02-19 14:01:55 +0330

    BitCoinCash started

---
+++ xchainpy/xchainpy_bitcoincash/client.py
@@ -0,0 +1,9 @@
+class IBitcoinCashClient():
+    def derive_path(self) -> str :
+        pass
+    def get_fees_with_rates(self , memo = None):
+        pass
+    def get_fees_with_memo(self , memo = None):
+        pass
+    def get_fee_rates():
+        pass
