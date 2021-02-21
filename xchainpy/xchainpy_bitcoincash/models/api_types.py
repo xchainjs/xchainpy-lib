0496a6f1ac694569b4b9d1ae2d4877afaa4f0761
Adelapt98 <adel.aboutalebi@hotmail.com>
2021-02-22 00:06:22 +0330

    added Transaction , TransctionInput , TransactionOutput Models

--- xchainpy/xchainpy_bitcoincash/models/api_types.py
+++ xchainpy/xchainpy_bitcoincash/models/api_types.py
@@ -55,7 +55,246 @@ class AddressBalance:
     def confirmed(self, confirmed):
         self._confirmed = confirmed 
 
+class TransactionInput:
+    def __init__(self , pkscript , value , address , witness , sequence , output , sigscript , coinbase , txid):
+        self._pkscript = pkscript
+        self._value = value
+        self._address = address
+        self._witness = witness
+        self._sequence = sequence
+        self._output = output
+        self._sigscript = sigscript
+        self._coinbase = coinbase
+        self._txid = txid
+    
+    @property
+    def pkscript(self):
+        return self._pkscript
+
+    @pkscript.setter
+    def pkscript(self, pkscript):
+        self._pkscript = pkscript 
+    
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value 
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, address):
+        self._address = address 
+
+    @property
+    def witness(self):
+        return self._witness
+
+    @witness.setter
+    def witness(self, witness):
+        self._witness = witness 
+
+    @property
+    def sequence(self):
+        return self._sequence
+
+    @sequence.setter
+    def sequence(self, sequence):
+        self._sequence = sequence 
+    
+    @property
+    def output(self):
+        return self._output
+
+    @output.setter
+    def output(self, output):
+        self._output = output 
+
+    @property
+    def sigscript(self):
+        return self._sigscript
+
+    @sigscript.setter
+    def sigscript(self, sigscript):
+        self._sigscript = sigscript 
+
+    @property
+    def coinbase(self):
+        return self._coinbase
+
+    @coinbase.setter
+    def coinbase(self, coinbase):
+        self._coinbase = coinbase 
+
+    @property
+    def txid(self):
+        return self._txid
+
+    @txid.setter
+    def txid(self, txid):
+        self._txid = txid 
+
+class TransactionOutput:
+    def __init__(self ,spent , pkscript , value , address , spender):
+        self._spent = spent
+        self._pkscript = pkscript
+        self._value = value
+        self._address = address
+        self._spender = spender
+    
+    @property
+    def spent(self):
+        return self._spent
+
+    @spent.setter
+    def spent(self, spent):
+        self._spent = spent 
+
+    @property
+    def pkscript(self):
+        return self._pkscript
+
+    @pkscript.setter
+    def pkscript(self, pkscript):
+        self._pkscript = pkscript 
+    
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value 
+    
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, address):
+        self._address = address
+
+    @property
+    def spender(self):
+        return self._spender
+
+    @spender.setter
+    def spender(self, spender):
+        self._spender = spender
+
 
 class Transaction:
-    def __init__(self , time , size , inputs , weight , fee , locktime , block , outputs , version , deleted , rbf , txid):
-        pass
+    def __init__(self , time , size , inputs : TransactionInput , weight , fee , locktime , block , outputs : TransactionOutput , version , deleted , rbf , txid):
+        self._time = time
+        self._size = size
+        self._inputs = inputs
+        self._weight = weight
+        self._fee = fee
+        self._locktime = locktime
+        self._block = block
+        self._outputs = outputs
+        self._version = version
+        self._deleted = deleted
+        self._rbf = rbf
+        self._txid = txid
+        
+    @property
+    def time(self):
+        return self._time
+
+    @time.setter
+    def time(self, time):
+        self._time = time 
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        self._size = size
+
+    @property
+    def inputs(self):
+        return self._inputs
+
+    @inputs.setter
+    def inputs(self, inputs):
+        self._inputs = inputs
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight):
+        self._weight = weight
+
+    @property
+    def fee(self):
+        return self._fee
+
+    @fee.setter
+    def fee(self, fee):
+        self._fee = fee
+
+    @property
+    def locktime(self):
+        return self._locktime
+
+    @locktime.setter
+    def locktime(self, locktime):
+        self._locktime = locktime
+    
+    @property
+    def block(self):
+        return self._block
+
+    @block.setter
+    def block(self, block):
+        self._block = block
+
+    @property
+    def outputs(self):
+        return self._outputs
+
+    @outputs.setter
+    def outputs(self, outputs):
+        self._outputs = outputs
+
+    @property
+    def version(self):
+        return self._outputs
+
+    @version.setter
+    def version(self, version):
+        self._version = version
+
+    @property
+    def deleted(self):
+        return self._deleted
+
+    @deleted.setter
+    def deleted(self, deleted):
+        self._deleted = deleted
+
+    @property
+    def rbf(self):
+        return self._rbf
+
+    @rbf.setter
+    def rbf(self, rbf):
+        self._rbf = rbf
+
+    @property
+    def txid(self):
+        return self._txid
+
+    @txid.setter
+    def txid(self, txid):
+        self._txid = txid
