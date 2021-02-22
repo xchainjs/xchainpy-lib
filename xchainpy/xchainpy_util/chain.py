59d33ef0fb60ce67b10f8f7e47ed60421e2b80d0
Adel <adel.aboutalebi@hotmail.com>
2021-02-22 21:13:14 +0330

    add BCH chain

--- xchainpy/xchainpy_util/chain.py
+++ xchainpy/xchainpy_util/chain.py
@@ -10,11 +10,12 @@ COSMOSCHAIN = "GAIA"
 
 POLKADOTCHAIN = "POLKA"
 
+BITCOINCASH = "BCH"
+
 chains = ["BNB", "BTC", "ETH", "THOR", "GAIA", "POLKA"]
 
 def is_chain(chain):
     """Is the argument a chain or not
-
     :returns: True of False
     """
     return True if chain in chains else False
