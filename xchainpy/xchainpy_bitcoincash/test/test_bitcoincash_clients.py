c4088d0ed534b8b5ac39f1ad57e6b852423afe4d
Adel <adel.aboutalebi@hotmail.com>
2021-02-24 16:57:18 +0330

    add tests for fee methods and transfer

--- xchainpy/xchainpy_bitcoincash/test/test_bitcoincash_clients.py
+++ xchainpy/xchainpy_bitcoincash/test/test_bitcoincash_clients.py
@@ -1,7 +1,6 @@
 import pytest
 from xchainpy.xchainpy_bitcoincash.client import Client
 from xchainpy.xchainpy_util.asset import Asset
-# from xchainpy.xchainpy_client.models import tx_types
 
 
 class TestBitcoincashClient:
@@ -55,123 +54,52 @@ class TestBitcoincashClient:
         assert len(tx_data.tx_to) == 2
         assert tx_data.tx_to[0].address == 'bchtest:qq235k7k9y5cwf3s2vfpxwgu8c5497sxnsdnxv6upc'
         assert str(tx_data.tx_to[0].amount) == '0.04005704'
-    
 
-    # @pytest.mark.asyncio
-    # async def test_equal_balances_when_call_getbalance_twice(self, client):
-    #     balance1 = await self.client.get_balance()
-    #     balance2 = await self.client.get_balance()
-    #     assert balance1.amount == balance2.amount
 
-    # @pytest.mark.asyncio
-    # async def test_transfer_with_memo_and_fee_rate(self):
-    #     self.client = Client(self.phrase_for_tx1, network='testnet')
-    #     fee_rates = await self.client.get_fee_rates()
-    #     fee_rate = fee_rates['fast']
-    #     balance = await self.client.get_balance()
-    #     if balance.amount > 0:
-    #         amount = 0.0000001
-    #         tx_id = await self.client.transfer(amount, self.testnetaddress_for_tx2, self.memo, fee_rate)
-    #         assert tx_id
-    #     self.client.purge_client()
+    @pytest.mark.asyncio
+    async def test_transfer_with_memo_and_fee_rate(self, client):
+        fee_rates = await self.client.get_fee_rates()
+        fee_rate = fee_rates['fast']
+        balance = await self.client.get_balance()
+        if balance.amount > 0:
+            amount = 0.00000001
+            tx_id = await self.client.transfer(amount, 'bchtest:qzt6sz836wdwscld0pgq2prcpck2pssmwge9q87pe9', self.memo, fee_rate)
+            assert tx_id
+            
+    @pytest.mark.asyncio
+    async def test_fee_and_rates_normal_tx(self, client):
+        fees_and_rates = await self.client.get_fees_with_rates()
+        fees = fees_and_rates['fees']
+        rates = fees_and_rates['rates']
+        assert fees['fastest']
+        assert fees['fast']
+        assert fees['average']
+        assert rates['fastest']
+        assert rates['fast']
+        assert rates['average']
 
-    # @pytest.mark.asyncio
-    # async def test_purge_client_should_purge_phrase_and_utxos(self):
-    #     self.client = Client(self.phrase, network='testnet')
-    #     self.client.purge_client()
-    #     with pytest.raises(Exception) as err:
-    #         self.client.get_address()
-    #     assert str(err.value) == "Phrase must be provided"
-    #     with pytest.raises(Exception) as err:
-    #         await self.client.get_balance()
-    #     assert str(err.value) == "Phrase must be provided"
+    @pytest.mark.asyncio
+    async def test_fee_and_rates_with_memo(self, client):
+        fees_and_rates = await self.client.get_fees_with_rates(self.memo)
+        fees = fees_and_rates['fees']
+        rates = fees_and_rates['rates']
+        assert fees['fastest']
+        assert fees['fast']
+        assert fees['average']
+        assert rates['fastest']
+        assert rates['fast']
+        assert rates['average']
 
-    # @pytest.mark.asyncio
-    # async def test_should_prevent_tx_when_fees_and_valueOut_exceed_balance(self, client):
-    #     balance = await self.client.get_balance()
-    #     if balance.amount > 0:
-    #         amount = balance.amount + 1000  # BTC
-    #         with pytest.raises(Exception) as err:
-    #             await self.client.transfer(amount, self.testnetaddress_for_tx2)
-    #         assert str(err.value) == "Balance insufficient for transaction"
+    @pytest.mark.asyncio
+    async def test_estimated_fees_normal_tx(self, client):
+        fees = await self.client.get_fees()
+        assert fees['fastest']
+        assert fees['fast']
+        assert fees['average']
 
-    # @pytest.mark.asyncio
-    # async def test_fee_and_rates_normal_tx(self, client):
-    #     fees_and_rates = await self.client.get_fees_with_rates()
-    #     fees = fees_and_rates['fees']
-    #     rates = fees_and_rates['rates']
-    #     assert fees['fastest']
-    #     assert fees['fast']
-    #     assert fees['average']
-    #     assert rates['fastest']
-    #     assert rates['fast']
-    #     assert rates['average']
+    @pytest.mark.asyncio
+    async def test_different_fees_normal_tx(self, client):
+        fees = await self.client.get_fees()
 
-    # @pytest.mark.asyncio
-    # async def test_fee_and_rates_with_memo(self, client):
-    #     fees_and_rates = await self.client.get_fees_with_rates(self.memo)
-    #     fees = fees_and_rates['fees']
-    #     rates = fees_and_rates['rates']
-    #     assert fees['fastest']
-    #     assert fees['fast']
-    #     assert fees['average']
-    #     assert rates['fastest']
-    #     assert rates['fast']
-    #     assert rates['average']
-
-    # @pytest.mark.asyncio
-    # async def test_estimated_fees_normal_tx(self, client):
-    #     fees = await self.client.get_fees()
-    #     assert fees['fastest']
-    #     assert fees['fast']
-    #     assert fees['average']
-
-    # @pytest.mark.asyncio
-    # async def test_normal_tx_fees_and_vault_tx_fees(self, client):
-    #     normal_tx = await self.client.get_fees()
-    #     vault_tx = await self.client.get_fees_with_memo(self.memo)
-
-    #     if vault_tx['average'] > MIN_TX_FEE:
-    #         assert vault_tx['average'] > normal_tx['average']
-    #     else:
-    #         assert vault_tx['average'] == MIN_TX_FEE
-
-    # @pytest.mark.asyncio
-    # async def test_different_fees_normal_tx(self, client):
-    #     fees = await self.client.get_fees()
-
-    #     assert fees['fastest'] > fees['fast']
-    #     assert fees['fast'] > fees['average']
-
-    # @pytest.mark.asyncio
-    # async def test_has_balances_invalid_address(self, client):
-    #     with pytest.raises(Exception) as err:
-    #         await self.client.get_balance(address='invalid address')
-    #     assert str(err.value) == "Invalid address"
-
-    # @pytest.mark.asyncio
-    # async def test_transfer_invalid_address(self, client):
-    #     balance = await self.client.get_balance()
-    #     if balance.amount > 0:
-    #         amount = 0.0000001
-    #         with pytest.raises(Exception) as err:
-    #             await self.client.transfer(amount, 'invalid address')
-    #         assert str(err.value) == "Invalid address"
-
-    # @pytest.mark.asyncio
-    # async def test_get_transactions(self, client):
-    #     txs = await self.client.get_transactions({'address': self.address_for_transactions, 'limit': 4})
-    #     assert txs
-    #     if txs['total'] > 0:
-    #         tx = txs['tx'][0]
-    #         assert tx.asset == self.btc_asset
-    #         assert tx.tx_date
-    #         assert tx.tx_hash
-    #         assert tx.tx_type == 'transfer'
-    #         assert len(tx.tx_to)
-    #         assert len(tx.tx_from)
-
-    # @pytest.mark.asyncio
-    # async def test_get_transactions_limit_should_work(self, client):
-    #     txs = await self.client.get_transactions({'address': self.address_for_transactions, 'limit': 1})
-    #     assert len(txs['tx']) == 1    
+        assert fees['fastest'] > fees['fast']
+        assert fees['fast'] > fees['average']
