3f25183dc54862eb99ab70254f8b6a82f5a0ffe6
Adel <adel.aboutalebi@hotmail.com>
2021-04-10 12:36:54 +0430

    change crypto lib version

--- xchainpy/xchainpy_crypto/setup.py
+++ xchainpy/xchainpy_crypto/setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='xchainpy_crypto',
     packages=find_packages(),
-    version='0.1.3',
+    version='0.1.4',
     license='MIT',
     description='XCHAIN-CRYPTO encrypts a master phrase to a keystore',
     author='THORChain',
