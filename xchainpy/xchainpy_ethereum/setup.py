b127b6915bd922f8612cab64b58b6c1164441009
hoodieonwho <>
2021-04-15 22:21:19 +0200

    version changed to 0.2.1

--- xchainpy/xchainpy_ethereum/setup.py
+++ xchainpy/xchainpy_ethereum/setup.py
@@ -13,7 +13,7 @@ setup(
         "xchainpy_ethereum": ["resources/*"],
     },
     include_package_data=True,
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description='Custom Ethereum client and utilities used by XChainPY clients',
     author='THORChain',
