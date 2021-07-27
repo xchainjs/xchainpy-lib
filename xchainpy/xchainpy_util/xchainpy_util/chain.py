ca24d4cddd453c7d7c615ce5ff03cf0ff680e813
Adel <adel.aboutalebi@hotmail.com>
2021-07-27 15:27:20 +0430

    Chains renamed

--- xchainpy/xchainpy_util/xchainpy_util/chain.py
+++ xchainpy/xchainpy_util/xchainpy_util/chain.py
@@ -1,17 +1,21 @@
+import enum
+class Chain(enum.Enum):
+    Binance = "BNB"
+    Bitcoin = "BTC"
+    Ethereum = "ETH"
+    THORChain = "THOR"
+    Cosmos = "GAIA"
+    Polkadot = "POLKA"
+    BitcoinCash = "BCH"
+    Litecoin = "LTC"
+
 BNBCHAIN = "BNB"
-
 BTCCHAIN = "BTC"
-
 ETHCHAIN = "ETH"
-
 THORCHAIN = "THOR"
-
 COSMOSCHAIN = "GAIA"
-
 POLKADOTCHAIN = "POLKA"
-
 BITCOINCASH = "BCH"
-
 LTCCHAIN = "LTC"
 
 chains = ["BNB", "BTC", "ETH", "THOR", "GAIA", "POLKA", "BCH", "LTC"]
