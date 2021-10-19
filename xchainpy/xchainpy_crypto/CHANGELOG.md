1b687922fd53c6805179d1e0aa060834e0f031c8
adelapt98 <adelapt98@adelapt98s-MacBook-Pro.local>
2021-10-19 09:03:41 +0330

    update changelog file and setup.py

--- xchainpy/xchainpy_crypto/CHANGELOG.md
+++ xchainpy/xchainpy_crypto/CHANGELOG.md
@@ -2,6 +2,17 @@
 
 All notable changes to this project will be documented in this file.
 
+## 2021-10-18
+
+### Added
+
+- Added installation instructions to the Readme file
+
+### Fixed
+
+- Fixed validate_phrase bug
+
+
 ## 2021-08-5
 
 ### Fixed
--- xchainpy/xchainpy_crypto/setup.py
+++ xchainpy/xchainpy_crypto/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='xchainpy_crypto',
     packages=find_packages(),
-    version='0.1.7',
+    version='0.1.8',
     license='MIT',
     description='XCHAIN-CRYPTO encrypts a master phrase to a keystore',
     author='THORChain',
