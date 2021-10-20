9c6a2dd05c50f5b53a314dad3615c0cf202001fa
adelapt98 <adelapt98@adelapt98s-MacBook-Pro.local>
2021-10-20 12:49:46 +0330

    Added installation instructions to the Readme file

--- xchainpy/xchainpy_bitcoin/CHANGELOG.md
+++ xchainpy/xchainpy_bitcoin/CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to this project will be documented in this file.
 
+## 2021-10-20
 
+### Added
 
+- Added installation instructions to the Readme file
+
+### Fixed
+
+- Fixed a bug in tests
 
--- xchainpy/xchainpy_bitcoin/setup.py
+++ xchainpy/xchainpy_bitcoin/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='xchainpy_bitcoin',
     packages=find_packages(),
-    version='0.2.2',
+    version='0.2.3',
     license='MIT',
     description='Bitcoin Module for XChainPy Clients',
     author='THORChain',
@@ -15,14 +15,14 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/xchainjs/xchainpy-lib',
     keywords=["THORChain", "XChainpy","XChainpy_bitcoin","BTC","Bitcoin"],
-    install_requires=['bitcoinlib>=0.5.2', 'http3>=0.6.7', 'xchainpy_client>=0.1.6', 'xchainpy_crypto>=0.1.7', 'xchainpy_util>=0.1.8', 'bip_utils>=1.11.1'],
+    install_requires=['bitcoinlib>=0.5.2', 'http3>=0.6.7', 'xchainpy_client>=0.1.6', 'xchainpy_crypto>=0.1.11', 'xchainpy_util>=0.1.8', 'bip_utils==1.11.1'],
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python'
     ],
 )
